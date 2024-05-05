4967b67941efe2e488d22d612da1ecf77daf0558
SergeiSmetanin1 <sergejsmetanin11@gmail.com>
2024-05-06 03:13:12 +0600

    add serch

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
+const main = document.querySelector('.main'); 
 const allCharactersContainer = document.querySelector(".characters-all");
 const moreCharactersBtn = document.querySelector("#moreCharacters");
 const informationCharacters = document.querySelector(".information-characters");
 const containerInfo = document.querySelector(".container-info");
-const searchHeader = document.querySelector(".search-header");
+const inputHeader = document.querySelector('.input-header');
+const resultSearchContainer = document.querySelector(".search-container");
+const loader = document.getElementById('loader');
 
 const URL = "https://gateway.marvel.com:443/";
 const publicApiKey = "da07fc4da480f7252b7a2c23dad22b62";
@@ -11,32 +14,50 @@ let limitCharacters = 8;
 let offsetCharacters = 0;
 
 const getData = async (limit, offset) => {
-    const res = await fetch(`${URL}v1/public/characters?limit=${limit}&offset=${offset}&apikey=${publicApiKey}`, {
-        method: "GET"
-    })
+    // Показываем загрузчик
+    showLoader();
 
-    if (res.ok) {
-        const data = await res.json();
-        console.log(data.data.results);
-        const imgData = data.data.results;
-        imgData.forEach(element => {
-            // console.log(`${element.thumbnail.path}.jpg`);
-            if (element.thumbnail.path === " ") {
-                console.log(element.thumbnail.path);
-                return
-            }
-            const character = `
-                <div class="characters-block" data-id="${element.id}">
-                    <img src="${element.thumbnail.path}.${element.thumbnail.extension}" alt="#">
-                    <div class="characters-block-description">
-                        <h3>${element.name}</h3>
-                    </div>
-                </div>
-            `;
-            allCharactersContainer.insertAdjacentHTML("beforeend", character);
+    try {
+        const res = await fetch(`${URL}v1/public/characters?limit=${limit}&offset=${offset}&apikey=${publicApiKey}`, {
+            method: "GET"
         });
+
+        if (res.ok) {
+            const data = await res.json();
+            const imgData = data.data.results;
+            imgData.forEach(element => {
+                if (element.thumbnail.path === " ") {
+                    console.log(element.thumbnail.path);
+                    return;
+                }
+                const character = `
+                    <div class="characters-block" data-id="${element.id}">
+                        <img src="${element.thumbnail.path}.${element.thumbnail.extension}" alt="${element.name}">
+                        <div class="characters-block-description">
+                            <h3>${element.name}</h3>
+                        </div>
+                    </div>
+                `;
+                allCharactersContainer.insertAdjacentHTML("beforeend", character);
+            });
+        }
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    } finally {
+        // Скрываем загрузчик после получения ответа (успешного или с ошибкой)
+        hideLoader();
     }
 }
+
+const showLoader = () => {
+    loader.style.display = 'block'; // Показываем загрузчик
+}
+
+// Функция для скрытия загрузчика
+const hideLoader = () => {
+    loader.style.display = 'none'; // Скрываем загрузчик
+}
+
 const getDataCharacter = async (id) => {
     const res = await fetch(`${URL}v1/public/characters/${id}?apikey=${publicApiKey}`, {
         method: "GET"
@@ -44,8 +65,6 @@ const getDataCharacter = async (id) => {
 
     if (res.ok) {
         const data = await res.json();
-        console.log(data.data.results[0].name);
-        console.log(data.data.results[0]);
         const stories = data.data.results[0].stories.items;
         informationCharacters.innerHTML = "";
         containerInfo.classList.add("shadow");
@@ -53,7 +72,7 @@ const getDataCharacter = async (id) => {
 
         const item = `
             <span class="close-button">×</span>
-            <img src="${data.data.results[0].thumbnail.path}.${data.data.results[0].thumbnail.extension}" alt="character-img">
+            <img src="${data.data.results[0].thumbnail.path}.${data.data.results[0].thumbnail.extension}" alt="${data.data.results[0].name}">
             <h3>${data.data.results[0].name}</h3>
             <p>${data.data.results[0].description}</p>
             <ul class="characters-comics-list" id="comicsList">
@@ -71,7 +90,6 @@ const getDataCharacter = async (id) => {
     }
 }
 
-
 const getCharacter = (e) => {
     const target = e.target;
     const charactersBlock = target.closest(".characters-block");
@@ -81,11 +99,41 @@ const getCharacter = (e) => {
     }
 }
 
-const searchCharacters = () => {
-    
+const getSearchData = async () => {
+    if (!inputHeader.value) {
+        resultSearchContainer.classList.add("hide");
+        return;
+    }
+
+    const res = await fetch(`${URL}v1/public/characters?apikey=${publicApiKey}&nameStartsWith=${inputHeader.value}&limit=100`, {
+        method: 'GET'
+    })
+
+    if (res.ok) {
+        const data = await res.json();
+        resultSearchContainer.innerHTML = "";
+        resultSearchContainer.classList.remove("hide");
+        const results = data.data.results;
+        results.forEach(element => {
+            const button = document.createElement("span");
+            button.classList.add("btnInfo");
+            button.dataset.characterId = element.id;
+            button.textContent = element.name; 
+            button.addEventListener("click", (e) => {
+                getDataCharacter(button.dataset.characterId);
+            });
+            resultSearchContainer.appendChild(button);
+        });
+    }
 }
 
-getData(limitCharacters);
+const mainContainer = (e) => {
+    const target = e.target;
+    if (!target.classList.contains("btnInfo")) {
+        resultSearchContainer.classList.add("hide");
+    }
+}
+
 
 moreCharactersBtn.addEventListener("click", () => {
     offsetCharacters = offsetCharacters + 8;
@@ -97,10 +145,13 @@ containerInfo.addEventListener("click", (e) => {
     const target = e.target;
     const cardInfo = target.closest(".information-characters");
     if (!cardInfo) {
-        console.log(target);
         informationCharacters.innerHTML = "";
         containerInfo.classList.remove("shadow");
         informationCharacters.classList.add("hide");
     }
+
 })
-searchHeader.addEventListener("click", searchCharacters)
+inputHeader.addEventListener('input', getSearchData);
+inputHeader.addEventListener("click", getSearchData);
+main.addEventListener("click", mainContainer);
+getData(limitCharacters);
