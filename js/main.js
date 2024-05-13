8130277e278d79bfc804eb1e57817da797819392
SergeiSmetanin1 <sergejsmetanin11@gmail.com>
2024-05-14 00:34:22 +0600

    final

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 const main = document.querySelector('.main'); 
+const charactersSection = document.querySelector(".characters");
+const comicsSection = document.querySelector(".comics");
 const allCharactersContainer = document.querySelector(".characters-all");
 const moreCharactersBtn = document.querySelector("#moreCharacters");
+const moreComicsBtn = document.querySelector("#moreComics");
 const informationCharacters = document.querySelector(".information-characters");
 const containerInfo = document.querySelector(".container-info");
 const inputHeader = document.querySelector('.input-header');
@@ -8,12 +11,15 @@ const resultSearchContainer = document.querySelector(".search-container");
 const loader = document.getElementById('loader');
 const charactersSwitch = document.querySelector("#characters-switch");
 const comicsSwitch = document.querySelector("#comics-switch");
+const allComicsContainer = document.querySelector(".comics-all");
 
 const URL = "https://gateway.marvel.com:443/";
 const publicApiKey = "da07fc4da480f7252b7a2c23dad22b62";
 
 let limitCharacters = 8;
 let offsetCharacters = 0;
+let limitComics = 8;
+let offsetComics = 0;
 
 const getData = async (limit, offset) => {
     showLoader();
@@ -28,7 +34,6 @@ const getData = async (limit, offset) => {
             const imgData = data.data.results;
             imgData.forEach(element => {
                 if (element.thumbnail.path === " ") {
-                    console.log(element.thumbnail.path);
                     return;
                 }
                 const character = `
@@ -57,7 +62,7 @@ const hideLoader = () => {
     loader.style.display = 'none'; 
 }
 
-const getDataCharacter = async (id) => {
+const getInfoCharacter = async (id) => {
     const res = await fetch(`${URL}v1/public/characters/${id}?apikey=${publicApiKey}`, {
         method: "GET"
     })
@@ -94,7 +99,7 @@ const getCharacter = (e) => {
     const charactersBlock = target.closest(".characters-block");
 
     if (charactersBlock) {
-        getDataCharacter(charactersBlock.dataset.id);
+        getInfoCharacter(charactersBlock.dataset.id);
     }
 }
 
@@ -119,7 +124,7 @@ const getSearchData = async () => {
             button.dataset.characterId = element.id;
             button.textContent = element.name; 
             button.addEventListener("click", (e) => {
-                getDataCharacter(button.dataset.characterId);
+                getInfoCharacter(button.dataset.characterId);
             });
             resultSearchContainer.appendChild(button);
         });
@@ -133,13 +138,80 @@ const mainContainer = (e) => {
     }
 }
 
+const getDataComics = async (limit, offset) => {
+    const res = await fetch(`${URL}v1/public/comics?limit=${limit}&offset=${offset}&apikey=${publicApiKey}`, {
+        method: "GET"
+    })
+
+    if (res.ok) {
+        const data = await res.json();
+        const imgData = data.data.results;
+        imgData.forEach(element => {
+            if (element.thumbnail.path === " ") {
+                return;
+            }
+            const character = `
+                <div class="characters-block" data-id="${element.id}">
+                    <img src="${element.thumbnail.path}.${element.thumbnail.extension}" alt="${element.title}">
+                    <div class="characters-block-description">
+                        <h3>${element.title}</h3>
+                    </div>
+                </div>
+            `;
+            allComicsContainer.insertAdjacentHTML("beforeend", character);
+        });
+    }
+}
+
+const getComicsInfo = async (id) => {
+    const res = await fetch(`https://gateway.marvel.com:443/v1/public/comics/${id}?apikey=${publicApiKey}`)
+    if (res.ok) {
+        const data = await res.json();
+        informationCharacters.innerHTML = "";
+        containerInfo.classList.add("shadow");
+        informationCharacters.classList.remove("hide");
+        informationCharacters.classList.add("col");
+
+        const item = `
+            <img src="${data.data.results[0].thumbnail.path}.${data.data.results[0].thumbnail.extension}" alt="${data.data.results[0].name}">
+            <span class="close-button">×</span>
+            <h3>Title: ${data.data.results[0].title}</h3>
+            <span>Creator: ${data.data.results[0].creators.items[0].name}</span>
+            <span>Price: ${data.data.results[0].prices[0].price}</span>
+            <span>Format: ${data.data.results[0].format}</span>
+            <span>Page: ${data.data.results[0].pageCount}</span>
+        `;
+        informationCharacters.insertAdjacentHTML("beforeend", item);
+        document.querySelector(".close-button").addEventListener("click", () => {
+            informationCharacters.innerHTML = "";
+            containerInfo.classList.remove("shadow");
+            informationCharacters.classList.add("hide");
+        });
+
+    }
+}
+
+const getComics = (e) => {
+    const target = e.target;
+    const comicsBlock = target.closest(".characters-block");
+
+    if (comicsBlock) {
+        getComicsInfo(comicsBlock.dataset.id);
+    }
+}
 
 moreCharactersBtn.addEventListener("click", () => {
-    offsetCharacters = offsetCharacters + 8;
+    offsetCharacters += 8;
     getData(limitCharacters, offsetCharacters);
+});
+
+moreComicsBtn.addEventListener("click", () => {
+    offsetComics += 8;
+    getDataComics(limitComics, offsetComics);
 })
 
 allCharactersContainer.addEventListener("click", getCharacter);
+allComicsContainer.addEventListener("click", getComics);
 containerInfo.addEventListener("click", (e) => {
     const target = e.target;
     const cardInfo = target.closest(".information-characters");
@@ -154,9 +226,13 @@ inputHeader.addEventListener('input', getSearchData);
 inputHeader.addEventListener("click", getSearchData);
 main.addEventListener("click", mainContainer);
 charactersSwitch.addEventListener("click", ()=>{
-    console.log(5);
+    charactersSection.classList.remove("hide");
+    comicsSection.classList.add("hide");
 });
 comicsSwitch.addEventListener("click", ()=>{
-    console.log(5);
+    charactersSection.classList.add("hide");
+    comicsSection.classList.remove("hide");
 });
+
 getData(limitCharacters);
+getDataComics(limitComics, offsetComics);
